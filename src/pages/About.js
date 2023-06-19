3eb704fbb1cd688b61cf35359a97740bf688f3d0
thuloj <91489322+thuloj@users.noreply.github.com>
2023-06-19 19:41:02 -0400

    updated about page, added all functionality

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,27 +1,36 @@
 import React, { Component } from "react";
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import profile_pic_aaryaman from "../assets/profile_pic_aaryaman.jpg";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img
+                className="profile_image"
+                src={profile_pic_aaryaman}
+                alt="Profile Pic - Aaryaman Thuloj"
+              ></img>
+            </div>
           </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Aaryaman Thuloj</div>
+              <div className="brief_description">
+                Hey guys! I'm a rising junior at Rensselaer Polytechnic
+                Institute studying Computer Science! I'm passionate about
+                software development and have interests in the following areas:{" "}
+                <ul>
+                  <li>Full Stack Web Development</li>
+                  <li>Artificial Intelligence</li>
+                  <li>Machine Learning</li>
+                  <li>Computational Finance</li>
+                </ul>
+                Excited to work with you all!
+              </div>
             </div>
           </div>
         </div>
